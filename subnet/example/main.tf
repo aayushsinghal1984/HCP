47eeffb6f0d076f359bb52dde4521b067012bb5f
aayushsinghal1984 <aayush.singhal01@gmail.com>
2026-08-04 10:31:12 +0530

    Update main.tf

--- subnet/example/main.tf
+++ subnet/example/main.tf
@@ -1,28 +1,3 @@
-terraform {
-  required_version = ">= 1.15.0"
-
-  cloud {
-    organization = "ULRI-ULSE"
-
-    workspaces {
-      name = "subnet-workspace"
-    }
-  }
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.70.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-  use_oidc = true
-}
-
-
 
 module "subnet" {
   source = "../"
